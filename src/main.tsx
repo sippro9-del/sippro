79a03414fbccf600c4d7e542bbd6a5a55e5fda98
sippro9-del <sippro9@gmail.com>
2026-03-26 21:48:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <App />
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  document.body.innerHTML = "<h2 style='color:red;text-align:center;'>Root not found</h2>";
+}
